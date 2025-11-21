b3b79a6bf6d5131e4a9bafed47589e32f0e01c55
Paloma Maciel Pereira dos Santos <paloma.maciel@aluno.ifsertao-pe.edu.br>
2025-11-21 17:41:59 -0300

    v1

--- DoList/settings.py
+++ DoList/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.tarefas',
-    'app.contas',
-    'app.core',
+    'apps.tarefas',
+    'apps.contas',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TarefasConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.tarefas'
+    name = 'apps.core'
